5fbf581decf4e1ad323b07920e0885da6b8647a8
GaryM <GaryM@b5f69653-2bf5-0310-8201-b0dafb8e1405>
2010-09-07 17:49:37 +0000

    [CellModelTests] Trying to get S1S2 to work (unsuccessfully).
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/projects/jonc/cell_model_tests@10259 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ test/S1S2Protocol.py
@@ -0,0 +1,41 @@
+"""Copyright (C) University of Oxford, 2005-2010
+
+University of Oxford means the Chancellor, Masters and Scholars of the
+University of Oxford, having an administrative office at Wellington
+Square, Oxford OX1 2JD, UK.
+
+This file is part of Chaste.
+
+Chaste is free software: you can redistribute it and/or modify it
+under the terms of the GNU Lesser General Public License as published
+by the Free Software Foundation, either version 2.1 of the License, or
+(at your option) any later version.
+
+Chaste is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+License for more details. The offer of Chaste under the terms of the
+License is subject to the License being interpreted in accordance with
+English Law and subject to any action against the University of Oxford
+being under the jurisdiction of the English Courts.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Chaste. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import protocol
+
+def apply_protocol(doc):
+    """
+    A very simple protocol, that doesn't make any changes, and is only
+    really interested in membrane_voltage as an output.
+    
+    However, for test coverage purposes, we also include each type of
+    variable in the outputs.
+    """
+    p = protocol.Protocol(doc.model, multi_stage=True)
+    V = doc.model.get_variable_by_oxmeta_name('membrane_voltage')
+    t = doc.model.get_variable_by_name('environment', 'time')
+    p.outputs = [V, t]
+    p.inputs = []
+    p.modify_model()
