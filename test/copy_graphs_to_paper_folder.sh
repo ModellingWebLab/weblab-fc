dbd2b7cd24b727f97977761d03dce34c40061417
GaryM <GaryM@b5f69653-2bf5-0310-8201-b0dafb8e1405>
2011-02-25 18:36:17 +0000

    [CellModelTests] Initial IV curves (2D sim), to be extended to 3D...
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/projects/jonc/cell_model_tests@11674 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- test/copy_graphs_to_paper_folder.sh
+++ test/copy_graphs_to_paper_folder.sh
@@ -6,3 +6,4 @@ CELL_MODEL_TESTS_SOURCE=`pwd`
 cd $CHASTE_TEST_OUTPUT
 
 cp CellModelTests/*/*/*_s1s2_curve.eps $CELL_MODEL_TESTS_SOURCE/papers/pbmb_model_interactions/images
+cp CellModelTests/*/*/*_ICaL_IV_curve.eps $CELL_MODEL_TESTS_SOURCE/papers/pbmb_model_interactions/images
