d3e3e45ad64bcbc42075d565d43425f5860c14e3
jonc@comlab.ox.ac.uk <jonc@comlab.ox.ac.uk@b5f69653-2bf5-0310-8201-b0dafb8e1405>
2013-07-24 19:57:00 +0000

    [FC] #2390 - outline a suitable base class for models converted from CellML.
    git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/projects/FunctionalCuration@19602 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- src/python/Math/Model.py
+++ src/python/Math/Model.py
@@ -78,8 +78,83 @@ class TestOdeModel(AbstractModel):
         env.DefineName('y', V.Simple(self.r.y))
         return env
 
-    
-    
+class AbstractOdeModel(AbstractModel):
+    """This is a base class for ODE system models converted from CellML by PyCml.
+
+    Subclasses must define three methods: __init__, EvaluateRhs and GetOutputs.
+    See their documentation here for details.
+      __init__: sets up self.stateVarMap, self.initialState, self.parameterMap and self.parameters
+      EvaluateRhs(self, t, y): returns a numpy array containing the derivatives at the given state
+      GetOutputs(self): returns an Environment
+
+    TODO: Figure out the neatest way for protocols to be able to access the current value of a model
+    output that isn't a state variable, parameter, or free variable.  (This is allowed in the C++ code.)
+    Given that a protocol is only able to do this at a state for which it can also obtain outputs, we
+    should probably cache the result of GetOutputs() for use in the ModelWrapperEnvironment.
+
+    TODO: Variable names set up by subclass constructors are just simple strings in the C++, assumed
+    to always live in the oxmeta namespace.  This constraint is still imposed by PyCml at present, but
+    we should move to use full URIs eventually.
+
+    TODO: Consider whether the ODE solver needs resetting (and how?) when a protocol changes the value
+    of a model parameter or state variable.
+    """
+    def __init__(self, *args, **kwargs):
+        """Construct a new ODE system model.
+
+        Before calling this constructor, subclasses should set up the following object attributes:
+         * initialState: a numpy array containing initial values for the state variables
+         * stateVarMap: a mapping from variable name to index within the state variable vector, for use in our ModelWrapperEnvironment
+         * parameters: a numpy array containing model parameter values
+         * parameterMap: a mapping from parameter name to index within the parameters vector, for use in our ModelWrapperEnvironment
+         * freeVariableName: the name of the free variable, for use in our ModelWrapperEnvironment
+
+        This method will initialise self.state to the initial model state, and set up the ODE solver.
+        """
+        super(AbstractOdeModel, self).__init__(*args, **kwargs)
+        self.state = self.initialState.copy()
+        self.solver = scipy.integrate.ode(self.EvaluateRhs)
+        self.SetFreeVariable(0) # A reasonable initial assumption; can be overridden by simulations
+        self.savedStates = {}
+
+    def EvaluateRhs(self, t, y):
+        """Compute the derivatives of the model.  This method must be implemented by subclasses.
+
+        :param t:  the free variable, typically time
+        :param y:  the ODE system state vector, a numpy array
+        """
+        raise NotImplementedError
+
+    def GetOutputs(self):
+        """Return an Environment containing the model outputs at its current state.
+
+        This method must be implemented by subclasses.
+        """
+        raise NotImplementedError
+
+    def SetFreeVariable(self, t):
+        """Set the value of the free variable (typically time), but retain the model's current state."""
+        self.freeVariable = t
+        self.solver.set_initial_value(self.state, self.freeVariable)
+
+    def SaveState(self, name):
+        """Save a copy of the current model state associated with the given name, to be restored using ResetState."""
+        self.savedStates[name] = self.state.copy()
+
+    def ResetState(self, name=None):
+        """Reset the model to the given named saved state, or to initial conditions if no name given."""
+        if name is None:
+            self.state = self.initialState.copy()
+        else:
+            self.state = self.savedStates[name].copy()
+        self.solver.set_initial_value(self.state, self.freeVariable)
+
+    def Simulate(self, endPoint):
+        """Simulate the model up to the given end point (value of the free variable)."""
+        self.state = self.solver.integrate(endPoint)
+        assert self.solver.successful()
+
+
 # for simulate time always starts from 0 and returns the integration of the differential equation 
 #at the point given
 # get outputs returns an environment where the inputs 
