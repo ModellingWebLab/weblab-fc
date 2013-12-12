ModellingWebLab/weblab-fc

commit 0a95cec4a6e635c0ba238bcc7333b6402c17512d

[FC] #2459 - make all ranges consistently update the unwrapped version of their value, and generally tidy up the code a bit.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/projects/FunctionalCuration@20699 b5f69653-2bf5-0310-8201-b0dafb8e1405

src/python/fc/simulations/ranges.py
@@ -33,123 +33,137 @@ OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 from ..language import values as V
 
 class AbstractRange(V.Simple):
-    """Base class for ranges in the protocol language."""
+    """Base class for ranges in the protocol language.
+
+    Handles enforcing the update of the range's value in the simulation environment's Python bindings dictionary.
+    """
+    def __init__(self, name):
+        """Initialise the common range properties."""
+        self.name = name
+        self.count = 0
+        self._value = float('nan')
+        self.numberOfOutputs = 0
+        self.env = None
+
     def Initialise(self, env):
-        pass
+        """Called by the associated simulation when its environment is initialised.
+
+        This method should evaluate any expressions used to define the range.
+        """
+        self.env = env
 
     @property
     def value(self):
-        return self.current
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+        if self.env is not None:
+            self.env.unwrappedBindings[self.name] = value
+
+    @property
+    def unwrapped(self):
+        return self.value
+
+    @property
+    def current(self):
+        return self.value
 
     def GetCurrentOutputPoint(self):
-        return self.current
+        return self.value
 
     def GetCurrentOutputNumber(self):
         return self.count - 1
 
+    def GetNumberOfOutputPoints(self):
+        return self.numberOfOutputs
+
 
 class UniformRange(AbstractRange):
     def __init__(self, name, startExpr, endExpr, stepExpr):
-        self.name = name
+        super(UniformRange, self).__init__(name)
         self.startExpr = startExpr
         self.endExpr = endExpr
         self.stepExpr = stepExpr
-        self.current = float('nan')
-        self.count = 0
-        
+
     def __iter__(self):
         self.count = 0
-        self.current = self.start
+        self.value = self.start
         return self
-    
+
     def next(self):
-        if self.current >= self.end:
+        if self.value >= self.end:
             self.count = 0
             raise StopIteration
         else:
+            self.value = self.start + self.step * self.count
             self.count += 1
-            self.current = self.start + self.step * (self.count - 1)
-            return self.current
-        
+            return self.value
+
     def Initialise(self, env):
-        self.env = env
+        super(UniformRange, self).Initialise(env)
         self.start = self.startExpr.Evaluate(self.env).value
         self.step = self.stepExpr.Evaluate(self.env).value
         self.end = self.endExpr.Evaluate(self.env).value
-        self.current = self.start
-        
-    def GetNumberOfOutputPoints(self):
-        return (round(self.end-self.start)/self.step) + 1
+        self.value = self.start
+        self.numberOfOutputs = (round(self.end-self.start)/self.step) + 1
 
 
 class VectorRange(AbstractRange):
     def __init__(self, name, arrOrExpr):
-        self.name = name
+        super(VectorRange, self).__init__(name)
         if isinstance(arrOrExpr, V.Array):
             self.expr = None
             self.arrRange = arrOrExpr.array
-            self.current = self.arrRange[0]
+            self.value = self.arrRange[0]
+            self.numberOfOutputs = len(self.arrRange)
         else:
             self.expr = arrOrExpr
-            self.current = float('nan')
+            self.value = float('nan')
         self.count = 0
-    
+
     def Initialise(self, env):
-        self.env = env
+        super(VectorRange, self).Initialise(env)
         if self.expr:
             self.arrRange = self.expr.Evaluate(env).array
-            self.current = self.arrRange[0]
-        
+            self.value = self.arrRange[0]
+            self.numberOfOutputs = len(self.arrRange)
+
     def __iter__(self):
         self.count = 0
-        self.current = 0
+        self.value = 0
         return self
-    
+
     def next(self):
         if self.count >= len(self.arrRange):
             self.count = 0
             raise StopIteration
         else:
-            self.current = self.arrRange[self.count]
-            self.env.unwrappedBindings[self.name] = self.current
+            self.value = self.arrRange[self.count]
             self.count += 1
             return self.current
-        
-    def GetNumberOfOutputPoints(self):
-        return len(self.arrRange)
 
 
 class While(AbstractRange):
     def __init__(self, name, condition):
-        self.name = name
+        super(While, self).__init__(name)
         self.condition = condition
         self.count = 0
+        self.value = 0
         self.numberOfOutputs = 1000
 
-    @property
-    def current(self):
-        return self.count - 1
-    
     def __iter__(self):
         self.count = 0
         return self
-    
-    def Initialise(self, env):
-        self.env = env
-        self.env.bindings[self.name] = V.Simple(self.current)
-        self.env.unwrappedBindings[self.name] = self.current
 
     def next(self):
         self.count += 1
-        self.env.bindings[self.name] = self
-        self.env.unwrappedBindings[self.name] = self.current
+        self.value += 1
         if self.count >= self.numberOfOutputs:
             self.numberOfOutputs += 1000
         if self.count > 1 and not self.condition.Evaluate(self.env).value:
             self.numberOfOutputs = self.count - 1
             raise StopIteration
         else:
-            return self.current
-
-    def GetNumberOfOutputPoints(self):
-        return self.numberOfOutputs
+            return self.value

commit 09a4bd536b1788e43ba349d9b07e0747527c8e6b

[FC] #2459 - fix while loops broken by r20699, and environment test broken by r20698. Also tidy ranges slightly again.
git-svn-id: https://chaste.cs.ox.ac.uk/svn/chaste/projects/FunctionalCuration@20702 b5f69653-2bf5-0310-8201-b0dafb8e1405

src/python/fc/simulations/ranges.py
@@ -43,14 +43,19 @@ class AbstractRange(V.Simple):
         self.count = 0
         self._value = float('nan')
         self.numberOfOutputs = 0
-        self.env = None
+        # Set an initial empty environment so calls to set our value in constructors don't fail
+        # (since Initialise hasn't been called by our simulation yet)
+        from ..utility.environment import Environment
+        AbstractRange.Initialise(self, Environment())
 
     def Initialise(self, env):
         """Called by the associated simulation when its environment is initialised.
 
-        This method should evaluate any expressions used to define the range.
+        Here we define the range variable within the environment.
+        Subclasses should also evaluate any expressions used to define the range.
         """
         self.env = env
+        env.DefineName(self.name, self)
 
     @property
     def value(self):
@@ -59,19 +64,18 @@ class AbstractRange(V.Simple):
     @value.setter
     def value(self, value):
         self._value = value
-        if self.env is not None:
-            self.env.unwrappedBindings[self.name] = value
+        self.env.unwrappedBindings[self.name] = value
 
     @property
     def unwrapped(self):
-        return self.value
+        return self._value
 
     @property
     def current(self):
-        return self.value
+        return self._value
 
     def GetCurrentOutputPoint(self):
-        return self.value
+        return self._value
 
     def GetCurrentOutputNumber(self):
         return self.count - 1
@@ -150,11 +154,12 @@ class While(AbstractRange):
         super(While, self).__init__(name)
         self.condition = condition
         self.count = 0
-        self.value = 0
+        self.value = -1
         self.numberOfOutputs = 1000
 
     def __iter__(self):
         self.count = 0
+        self.value = -1
         return self
 
     def next(self):

src/python/fc/simulations/simulations.py
@@ -56,9 +56,9 @@ class AbstractSimulation(locatable.Locatable):
         except NameError:
             pass
 
-    def Initialise(self):
-        self.env.DefineName(self.range_.name, self.range_)
-        self.range_.Initialise(self.env)
+    def Initialise(self, initialiseRange=True):
+        if initialiseRange:
+            self.range_.Initialise(self.env)
         if isinstance(self.range_, R.While) and self.prefix:
             self.viewEnv = Env.Environment(allowOverwrite=True)
             self.env.SetDelegateeEnv(self.viewEnv, self.prefix)
@@ -193,7 +193,7 @@ class Nested(AbstractSimulation):
         self.nestedSim.Initialise()
         if self.trace:
             self.nestedSim.trace = True
-        super(Nested, self).Initialise()
+        super(Nested, self).Initialise(initialiseRange=False)
 
     def InternalRun(self):
         for t in self.range_:

test/python/TestEnvironment.py
@@ -63,12 +63,12 @@ class TestEnvironment(unittest.TestCase):
         fresh1 = env.FreshIdent()
         fresh2 = env2.FreshIdent()
         self.assertNotEqual(fresh1, fresh2)
-        self.assertEqual(sorted(env.DefinedNames()),sorted(env2.DefinedNames()))
+        self.assertEqual(sorted(env.DefinedNames()), sorted(env2.DefinedNames()))
         env.Clear()
         self.assertEqual(len(env),0)
         env.DefineName("one", one)
         self.assertEqual(env.LookUp("one"), one)
-                
+
     def TestOverwritingEnv(self):
         env = Env.Environment()
         one = V.Simple(1)
@@ -82,30 +82,30 @@ class TestEnvironment(unittest.TestCase):
         env.OverwriteDefinition("one", two)
         self.assertEqual(env.LookUp("one"), two)
         env.Remove("one")
-        self.assertRaises(ProtocolError, env.LookUp, "one") # item was removed
+        self.assertRaises(KeyError, env.LookUp, "one") # item was removed
         env.DefineName("one",one)
         self.assertEqual(env.LookUp("one"), one)
         self.assertRaises(ProtocolError, env.Remove, "three") # never added
-        
+
     def TestDelegation(self):
         root_env = Env.Environment()
         middle_env = Env.Environment(delegatee=root_env)
         top_env = Env.Environment()
         top_env.SetDelegateeEnv(middle_env)
-        
+
         name = "name"
         value = V.Simple(123.4)
         root_env.DefineName(name, value)
         self.assertEqual(top_env.LookUp(name), value)
-        
+
         value2 = V.Simple(432.1)
         middle_env.DefineName(name, value2)
         self.assertEqual(top_env.LookUp(name), value2)
-        
+
         value3 = V.Simple(6.5)
         top_env.DefineName(name, value3)
         self.assertEqual(top_env.LookUp(name), value3)
-        
+
     def TestPrefixedDelegation(self):
         root_env = Env.Environment()
         env_a = Env.Environment()
@@ -114,39 +114,39 @@ class TestEnvironment(unittest.TestCase):
         env_b.DefineName("B", V.Simple(2))
         root_env.SetDelegateeEnv(env_a, 'a')
         root_env.SetDelegateeEnv(env_b, 'b')
-        
+
         self.assertRaises(ProtocolError, env_a.DefineName, 'a:b', V.Simple(1))
         self.assertEqual(root_env.LookUp('a:A').value, 1)
         self.assertEqual(root_env.LookUp('b:B').value, 2)
-        self.assertRaises(ProtocolError, root_env.LookUp, 'a:n')
-        self.assertRaises(ProtocolError, root_env.LookUp, 'c:c')
-        
+        self.assertRaises(KeyError, root_env.LookUp, 'a:n')
+        self.assertRaises(KeyError, root_env.LookUp, 'c:c')
+
         env_aa = Env.Environment(delegatee=env_a)
         env_aa.DefineName('A', V.Simple(3))
         env_a.SetDelegateeEnv(env_aa, 'a')
         self.assertEqual(env_a.LookUp('a:A').value, 3)
         self.assertEqual(root_env.LookUp('a:a:A').value, 3)
-        
+
     def TestEvaluateExprAndStmt(self):
         # basic mathml function
         env = Env.Environment()
         expr_str = 'MathML:max(100, 115, 98)'
         self.assertEqual(env.EvaluateExpr(expr_str, env).value, 115)
-        
+
         # array creation
         arr = V.Array(np.arange(10))
         env.DefineName('arr', arr)
         expr_str = 'arr[1:2:10]'
         predicted = np.array([1, 3, 5, 7, 9])
         np.testing.assert_array_almost_equal(predicted, env.EvaluateExpr(expr_str, env).array)
-        
+
         # view of an array
         view_arr = V.Array(np.arange(10))
         env.DefineName('view_arr', view_arr)
         expr_str = 'view_arr[4]'
         predicted = np.array(4)
         np.testing.assert_array_almost_equal(env.EvaluateExpr(expr_str, env).array, predicted)
-        
+
         # statement list
         stmt_str = 'z = lambda a: a+2\nassert z(2) == 4'
         env.EvaluateStatement(stmt_str, env) # assertion built into list, no extra test needed