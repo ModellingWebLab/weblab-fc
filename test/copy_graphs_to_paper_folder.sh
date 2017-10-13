0a3d188543dfa97f0b9ebef20c2bfc584e056614
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2017-10-13 15:38:52 +0100

    Remove files not relevant for the pure Python backend

--- test/copy_graphs_to_paper_folder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-
-cd ..
-CELL_MODEL_TESTS_SOURCE=`pwd`
-cd $CHASTE_TEST_OUTPUT
-
-cp CellModelTests/*/*/*_s1s2_curve.eps $CELL_MODEL_TESTS_SOURCE/papers/pbmb_model_interactions/images
-cp CellModelTests/*/*/*_ICaL_IV_curve.eps $CELL_MODEL_TESTS_SOURCE/papers/pbmb_model_interactions/images
