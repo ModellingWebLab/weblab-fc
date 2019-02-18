9382b7b0c91bd47bc907ad6c38fccc1717f788af
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-02-18 14:22:49 +0000

    Temporarily loading cellmlmanip and fccodegen directly from their repos.

---
+++ requirements/fccodegen.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+pip install -e git://github.com/ModellingWebLab/cellmlmanip.git@feature/cellml-parsing#egg=cellmlmanip
+pip install -e git://github.com/ModellingWebLab/fccodegen@cellmlmanip-updates#egg=fccodegen
+
