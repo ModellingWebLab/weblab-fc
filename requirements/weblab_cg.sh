acb8fdfae24b688e52be8b32a5fb3bdd4dfef56f
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2019-04-18 16:47:06 +0100

    Updated required cg branch

--- requirements/weblab_cg.sh
+++ requirements/weblab_cg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 pip install -e git://github.com/ModellingWebLab/cellmlmanip.git@feature/cellml-parsing#egg=cellmlmanip
-pip install -e git://github.com/ModellingWebLab/weblab_cg@metadata-lookups#egg=weblab_cg
+pip install -e git://github.com/ModellingWebLab/weblab_cg#egg=weblab_cg
 
