df182aee04672b1182a87ae1b633d1153f0d8635
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2020-02-05 13:36:25 +0000

    Model.get_initial_value no longer exists in cellmlmanip

--- fc/code_generation.py
+++ fc/code_generation.py
@@ -2,14 +2,12 @@
 Methods for code generation (using jinja2 templates).
 """
 import jinja2
-import os
 import posixpath
 import sympy
 import time
 
 from cellmlmanip import transpiler
 from cellmlmanip.printer import Printer
-import fc
 
 # Add an `_exp` method to sympy, and tell cellmlmanip to create _exp objects instead of exp objects.
 # This prevents Sympy doing simplification (or canonicalisation) resulting in weird errors with exps in some cardiac
@@ -196,7 +194,7 @@ def create_weblab_model(path, class_name, model, outputs, parameters):
             'index': i,
             'var_name': symbol_name(state),
             'deriv_name': derivative_name(state),
-            'initial_value': model.get_initial_value(state),
+            'initial_value': state.initial_value,
             'var_names': model.get_ontology_terms_by_symbol(state, oxmeta),
         })
 
