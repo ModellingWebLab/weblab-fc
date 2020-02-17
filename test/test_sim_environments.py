b65727aba52f6a73a59732a6dbe2af1a35c4e962
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2020-02-17 16:37:19 +0000

    Add failing test case for #95

---
+++ test/test_sim_environments.py
@@ -0,0 +1,13 @@
+import os
+
+import fc
+
+
+def test_sim_environments():
+    """Run the test_sim_environments.txt test protocol."""
+    proto = fc.Protocol('test/protocols/test_sim_environments.txt')
+    proto.set_output_folder('test_sim_environments')
+    proto.set_model('test/models/luo_rudy_1991.cellml')
+    proto.run()
+    # Test assertions are within the protocol itself. Here we just check it produced output.
+    assert os.path.exists(os.path.join(proto.output_folder.path, 'output.h5'))
