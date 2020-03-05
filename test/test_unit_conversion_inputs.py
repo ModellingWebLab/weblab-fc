68d7f27ab97e2b69c2014461bb27861940709ac3
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-03-05 22:06:04 +0000

    Added test for unit conversion when changing initial value of a constant.

--- test/test_unit_conversion_inputs.py
+++ test/test_unit_conversion_inputs.py
@@ -7,22 +7,28 @@ import pytest
 import fc
 
 
-@pytest.mark.xfail(strict=True, reason='Unit conversion not implemented yet.')
+#@pytest.mark.xfail(strict=True, reason='Unit conversion not implemented yet.')
 def test_unit_conversion_inputs():
-    # Test unit conversion for the initial value of a state variable
+    # Test unit conversion for initial values of states and constants.
 
     proto_file = 'test/protocols/test_unit_conversion_inputs.txt'
     proto = fc.Protocol(proto_file)
     proto.set_output_folder('test_unit_conversion_inputs')
-
-    # This model has v in mV, with dv/dt=1 mV/ms, and v(0) = 2mV
-    # The protocol will change this to v(0) = 10 V, and then run for 5ms
-    proto.set_model('test/models/single_ode.cellml')
+    proto.set_model('test/data/simple_ode.cellml')
     proto.run()
 
-    # Check output
+    # Check initial value of constant was set correctly
+    # The model has Na in mM with Na(0)=0mM, and dNa/dt=n, where n has initial value 1 mM/ms
+    # The protocol will change this to 3 M/ms, and then run for 5ms
+    Na = proto.output_env.look_up('cytosolic_sodium_concentration').array
+    assert len(Na) == 2
+    assert Na[0] == 0
+    assert Na[1] == 15000
+
+    # Check initial value of state was set correctly
+    # The model has v in mV, with dv/dt=1 mV/ms, and v(0)=2mV
+    # The protocol will change this to v(0)=10 V, and then run for 5ms
     v = proto.output_env.look_up('membrane_voltage').array
-    print(v)
     assert len(v) == 2
     assert v[0] == 10
     assert v[1] == 10.005
