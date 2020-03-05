323b405f68de85b036c16cf4fadf065b8faa5337
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-03-05 22:42:55 +0000

    Added test for unit conversion via inputs combined with defines.

--- test/test_unit_conversion_inputs.py
+++ test/test_unit_conversion_inputs.py
@@ -7,13 +7,13 @@ import pytest
 import fc
 
 
-#@pytest.mark.xfail(strict=True, reason='Unit conversion not implemented yet.')
-def test_unit_conversion_inputs():
+@pytest.mark.xfail(strict=True, reason='Unit conversion not implemented yet.')
+def test_unit_conversion_inputs_initial_values():
     # Test unit conversion for initial values of states and constants.
 
-    proto_file = 'test/protocols/test_unit_conversion_inputs.txt'
+    proto_file = 'test/protocols/test_unit_conversion_inputs_initial_values.txt'
     proto = fc.Protocol(proto_file)
-    proto.set_output_folder('test_unit_conversion_inputs')
+    proto.set_output_folder('test_unit_conversion_inputs_initial_values')
     proto.set_model('test/data/simple_ode.cellml')
     proto.run()
 
@@ -32,4 +32,31 @@ def test_unit_conversion_inputs():
     assert len(v) == 2
     assert v[0] == 10
     assert v[1] == 10.005
+
+
+@pytest.mark.xfail(strict=True, reason='Unit conversion not implemented yet.')
+def test_unit_conversion_inputs_defines():
+    # Test unit conversion for initial values of states and constants.
+
+    proto_file = 'test/protocols/test_unit_conversion_inputs_defines.txt'
+    proto = fc.Protocol(proto_file)
+    proto.set_output_folder('test_unit_conversion_inputs_defines')
+    proto.set_model('test/data/simple_ode.cellml')
+    proto.run()
+
+    # Check initial value of constant was set correctly
+    # The model has Na in mM with Na(0)=0mM, and dNa/dt=n, where n has initial value 1 mM/ms
+    # The protocol will change this to 5 M/ms, and then run for 5ms
+    Na = proto.output_env.look_up('cytosolic_sodium_concentration').array
+    assert len(Na) == 2
+    assert Na[0] == 0
+    assert Na[1] == 25000
+
+    # Check initial value of state was set correctly
+    # The model has v in mV, with dv/dt=1 mV/ms, and v(0)=2mV
+    # The protocol will change this to dv/dt=3 V/ms, and then run for 5ms
+    v = proto.output_env.look_up('membrane_voltage').array
+    assert len(v) == 2
+    assert v[0] == 0
+    assert v[1] == 15
 
