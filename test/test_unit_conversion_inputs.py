da7fdb334f90b4b250c93bdb3af2e4ac1e021920
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-03-06 09:36:46 +0000

    Tweaks and lots of comment fixes to test_unit_conversion_inputs.py

--- test/test_unit_conversion_inputs.py
+++ test/test_unit_conversion_inputs.py
@@ -19,24 +19,20 @@ def test_unit_conversion_inputs_initial_values():
 
     # Check initial value of constant was set correctly
     # The model has Na in mM with Na(0)=0mM, and dNa/dt=n, where n has initial value 1 mM/ms
-    # The protocol will change this to 3 M/ms, and then run for 5ms
+    # The protocol will change n's initial value to 3 M/ms, and then run for 5ms
     Na = proto.output_env.look_up('cytosolic_sodium_concentration').array
-    assert len(Na) == 2
-    assert Na[0] == 0
-    assert Na[1] == 15000
+    assert list(Na) == [0, 15000]
 
     # Check initial value of state was set correctly
     # The model has v in mV, with dv/dt=1 mV/ms, and v(0)=2mV
-    # The protocol will change this to v(0)=10 V, and then run for 5ms
+    # The protocol will change V's units to volts, set v(0)=10 V, and then run for 5ms
     v = proto.output_env.look_up('membrane_voltage').array
-    assert len(v) == 2
-    assert v[0] == 10
-    assert v[1] == 10.005
+    assert list(v) == [10, 10.005]
 
 
 @pytest.mark.xfail(strict=True, reason='Unit conversion not implemented yet.')
 def test_unit_conversion_inputs_defines():
-    # Test unit conversion for initial values of states and constants.
+    # Test unit conversion for input variables (states and constants) modified with define statements.
 
     proto_file = 'test/protocols/test_unit_conversion_inputs_defines.txt'
     proto = fc.Protocol(proto_file)
@@ -44,19 +40,15 @@ def test_unit_conversion_inputs_defines():
     proto.set_model('test/data/simple_ode.cellml')
     proto.run()
 
-    # Check initial value of constant was set correctly
+    # Check RHS of constant was set correctly
     # The model has Na in mM with Na(0)=0mM, and dNa/dt=n, where n has initial value 1 mM/ms
-    # The protocol will change this to 5 M/ms, and then run for 5ms
+    # The protocol will define n=5M/ms, and then run for 5ms
     Na = proto.output_env.look_up('cytosolic_sodium_concentration').array
-    assert len(Na) == 2
-    assert Na[0] == 0
-    assert Na[1] == 25000
+    assert list(Na) == [0, 25000]
 
     # Check initial value of state was set correctly
     # The model has v in mV, with dv/dt=1 mV/ms, and v(0)=2mV
-    # The protocol will change this to dv/dt=3 V/ms, and then run for 5ms
+    # The protocol will change V's units to volts, define dV/dt=3V/ms, and then run for 5ms
     v = proto.output_env.look_up('membrane_voltage').array
-    assert len(v) == 2
-    assert v[0] == 0
-    assert v[1] == 15
+    assert list(v) == [0, 15]
 
