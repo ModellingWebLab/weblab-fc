9f722b3f65c4ab90c2738b7543b2efc70456d6c7
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-03-30 11:07:20 +0100

    Updated test_mathml_evaluation to use pytest.

--- test/test_mathml_evaluation.py
+++ test/test_mathml_evaluation.py
@@ -1,22 +1,16 @@
+"""Test implementation of voltage clamps.
 
+Note that the first two cases in TestClamping.hpp are covered in TestAlgebraicModels.py!
+"""
 import os
-import pytest
-import unittest
 
 import fc
 
 
-class TestMathmlEvaluation(unittest.TestCase):
-    """Test implementation of voltage clamps in the Python code.
-
-    Note that the first two cases in TestClamping.hpp are covered in TestAlgebraicModels.py!
-    """
-
-    @pytest.mark.xfail(strict=True, reason='no pycml replacement yet')
-    def test_mathml_operations(self):
-        proto = fc.Protocol('test/protocols/test_mathml_evaluation.txt')
-        proto.set_output_folder('Py_TestMathmlEvaluation_TestMathmlOperations')
-        proto.set_model('cellml/beeler_reuter_model_1977.cellml')
-        proto.run()
-        # Test assertions are within the protocol itself
-        self.assertTrue(os.path.exists(os.path.join(proto.output_folder.path, 'output.h5')))
+def test_mathml_operations():
+    proto = fc.Protocol('test/protocols/test_mathml_evaluation.txt')
+    proto.set_output_folder('Py_TestMathmlEvaluation_TestMathmlOperations')
+    proto.set_model(os.path.join('test', 'models', 'beeler_reuter_model_1977.cellml'))
+    proto.run()
+    # Test assertions are within the protocol itself
+    assert os.path.exists(os.path.join(proto.output_folder.path, 'output.h5'))
