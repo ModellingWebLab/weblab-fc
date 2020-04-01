9ece8a170419cca18c256aa45856cd1105382601
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-04-01 12:23:39 +0100

    Rewrote test_clamping for pytest.

--- test/test_clamping.py
+++ test/test_clamping.py
@@ -1,42 +1,40 @@
+"""Test implementation of voltage clamps in the Python code.
 
+Note that the first two cases in TestClamping.hpp are covered in TestAlgebraicModels.py!
+"""
 import os
 import pytest
-import unittest
 
 import fc
 
 
-class TestClamping(unittest.TestCase):
-    """Test implementation of voltage clamps in the Python code.
+@pytest.mark.xfail(strict=True, reason='no pycml replacement yet')
+def test_clamping_with_variable_units():
+    proto = fc.Protocol('test/protocols/test_clamping3.txt')
+    proto.set_output_folder('Py_TestClamping_TestClampingWithVariableUnits')
+    proto.set_model('cellml/beeler_reuter_model_1977.cellml')
+    proto.run()
+    # Test assertions are within the protocol itself
+    assert os.path.exists(os.path.join(proto.output_folder.path, 'output.h5'))
 
-    Note that the first two cases in TestClamping.hpp are covered in TestAlgebraicModels.py!
-    """
 
-    @pytest.mark.xfail(strict=True, reason='no pycml replacement yet')
-    def test_clamping_with_variable_units(self):
-        proto = fc.Protocol('test/protocols/test_clamping3.txt')
-        proto.set_output_folder('Py_TestClamping_TestClampingWithVariableUnits')
-        proto.set_model('cellml/beeler_reuter_model_1977.cellml')
-        proto.run()
-        # Test assertions are within the protocol itself
-        self.assertTrue(os.path.exists(os.path.join(proto.output_folder.path, 'output.h5')))
+@pytest.mark.xfail(strict=True, reason='no pycml replacement yet')
+def test_clamp_to_data_file():
+    proto_file = 'protocols/timecourse_voltage_clamp.txt'
+    proto = fc.Protocol(proto_file)
+    proto.set_output_folder('Py_TestClamping_TestClampToDataFile')
+    proto.set_model('cellml/ten_tusscher_model_2004_epi.cellml')
+    proto.run()
+    # Test assertions are within the protocol itself
+    assert os.path.exists(os.path.join(proto.output_folder.path, 'output.h5'))
 
-    @pytest.mark.xfail(strict=True, reason='no pycml replacement yet')
-    def test_clamp_to_data_file(self):
-        proto_file = 'protocols/timecourse_voltage_clamp.txt'
-        proto = fc.Protocol(proto_file)
-        proto.set_output_folder('Py_TestClamping_TestClampToDataFile')
-        proto.set_model('cellml/ten_tusscher_model_2004_epi.cellml')
-        proto.run()
-        # Test assertions are within the protocol itself
-        self.assertTrue(os.path.exists(os.path.join(proto.output_folder.path, 'output.h5')))
 
-    @pytest.mark.xfail(strict=True, reason='no pycml replacement yet')
-    def test_interpolation_clamp(self):
-        proto_file = 'test/protocols/test_model_interpolation.txt'
-        proto = fc.Protocol(proto_file)
-        proto.set_output_folder('Py_TestClamping_TestInterpolationClamp')
-        proto.set_model('test/data/simple_ode.cellml')
-        proto.run()
-        # Test assertions are within the protocol itself
-        self.assertTrue(os.path.exists(os.path.join(proto.output_folder.path, 'output.h5')))
+@pytest.mark.xfail(strict=True, reason='no pycml replacement yet')
+def test_interpolation_clamp():
+    proto_file = 'test/protocols/test_model_interpolation.txt'
+    proto = fc.Protocol(proto_file)
+    proto.set_output_folder('Py_TestClamping_TestInterpolationClamp')
+    proto.set_model('test/data/simple_ode.cellml')
+    proto.run()
+    # Test assertions are within the protocol itself
+    assert os.path.exists(os.path.join(proto.output_folder.path, 'output.h5'))
