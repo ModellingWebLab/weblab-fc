18e061cde3de3f5ecbd3831d4a3586a594245e79
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-04-20 17:03:05 +0100

    Converting final tests to pytest.

--- test/test_algebraic_models.py
+++ test/test_algebraic_models.py
@@ -1,22 +1,23 @@
+"""
+Test behaviour on models with no ODEs; based on TestClamping.hpp
+"""
 
 import unittest
 
 import fc
 
 
-class TestAlgebraicModels(unittest.TestCase):
-    """Test behaviour on models with no ODEs; based on TestClamping.hpp"""
+def test_clamping_to_initial_value():
+    proto = fc.Protocol('test/protocols/test_clamping1.txt')
+    proto.set_output_folder('Py_TestAlgebraicModels_TestClampingToInitialValue')
+    proto.set_model('test/models/beeler_reuter_model_1977.cellml')
+    proto.run()
+    # Test assertions are within the protocol itself
 
-    def test_clamping_to_initial_value(self):
-        proto = fc.Protocol('test/protocols/test_clamping1.txt')
-        proto.set_output_folder('Py_TestAlgebraicModels_TestClampingToInitialValue')
-        proto.set_model('test/models/beeler_reuter_model_1977.cellml')
-        proto.run()
-        # Test assertions are within the protocol itself
 
-    def test_clamping_to_fixed_value(self):
-        proto = fc.Protocol('test/protocols/test_clamping2.txt')
-        proto.set_output_folder('Py_TestAlgebraicModels_TestClampingToFixedValue')
-        proto.set_model('test/models/beeler_reuter_model_1977.cellml')
-        proto.run()
-        # Test assertions are within the protocol itself
+def test_clamping_to_fixed_value():
+    proto = fc.Protocol('test/protocols/test_clamping2.txt')
+    proto.set_output_folder('Py_TestAlgebraicModels_TestClampingToFixedValue')
+    proto.set_model('test/models/beeler_reuter_model_1977.cellml')
+    proto.run()
+    # Test assertions are within the protocol itself
