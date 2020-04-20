42cd7fa5c28d6dccd8ee908112ce8858f1314ad8
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-04-20 18:53:32 +0100

    Fixed test_relations.

--- test/test_relations.py
+++ test/test_relations.py
@@ -5,35 +5,35 @@ import fc.language.expressions as E
 import fc.language.values as V
 
 
-def test_eq(self):
+def test_eq():
     assert not E.Eq(E.Const(V.Simple(1)), E.Const(V.Simple(0))).evaluate({}).value
     assert E.Eq(E.Const(V.Simple(1)), E.Const(V.Simple(1))).evaluate({}).value
 
 
-def test_neq(self):
+def test_neq():
     assert E.Neq(E.Const(V.Simple(1)), E.Const(V.Simple(0))).evaluate({}).value
     assert not E.Neq(E.Const(V.Simple(1)), E.Const(V.Simple(1))).evaluate({}).value
 
 
-def test_lt(self):
+def test_lt():
     assert not E.Lt(E.Const(V.Simple(1)), E.Const(V.Simple(0))).evaluate({}).value
     assert E.Lt(E.Const(V.Simple(0)), E.Const(V.Simple(1))).evaluate({}).value
     assert not E.Lt(E.Const(V.Simple(1)), E.Const(V.Simple(1))).evaluate({}).value
 
 
-def test_gt(self):
+def test_gt():
     assert E.Gt(E.Const(V.Simple(1)), E.Const(V.Simple(0))).evaluate({}).value
     assert not E.Gt(E.Const(V.Simple(0)), E.Const(V.Simple(1))).evaluate({}).value
     assert not E.Gt(E.Const(V.Simple(1)), E.Const(V.Simple(1))).evaluate({}).value
 
 
-def test_leq(self):
+def test_leq():
     assert not E.Leq(E.Const(V.Simple(1)), E.Const(V.Simple(0))).evaluate({}).value
     assert E.Leq(E.Const(V.Simple(0)), E.Const(V.Simple(1))).evaluate({}).value
     assert E.Leq(E.Const(V.Simple(1)), E.Const(V.Simple(1))).evaluate({}).value
 
 
-def test_geq(self):
+def test_geq():
     assert E.Geq(E.Const(V.Simple(1)), E.Const(V.Simple(0))).evaluate({}).value
     assert not E.Geq(E.Const(V.Simple(0)), E.Const(V.Simple(1))).evaluate({}).value
     assert E.Geq(E.Const(V.Simple(1)), E.Const(V.Simple(1))).evaluate({}).value
