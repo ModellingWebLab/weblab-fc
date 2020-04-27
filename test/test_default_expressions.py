96ea97077590bc511cfa274e48de6a1472bda0db
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-04-27 15:33:49 +0100

    Added test for creating optional variables without units (should fail).

--- test/test_default_expressions.py
+++ test/test_default_expressions.py
@@ -1,7 +1,10 @@
 """
 Test default expressions for optional variables.
 """
+import pytest
+
 import fc
+from fc.error_handling import ProtocolError
 
 
 def test_optional_with_default_expression():
@@ -13,3 +16,14 @@ def test_optional_with_default_expression():
     proto.set_model('test/data/simple_ode.cellml')
     proto.run()
     # Assertions are within the protocol itself
+
+
+def test_optional_with_default_expression_but_no_units():
+    # Test creating an optional variable with a default expression, but without enough information to create (no units)
+
+    proto_file = 'test/protocols/test_default_expression_no_units.txt'
+    proto = fc.Protocol(proto_file)
+    proto.set_output_folder('test_optional_with_default_expression_but_no_units')
+    with pytest.raises(ProtocolError, match='blaaaaaar'):
+        proto.set_model('test/data/simple_ode.cellml')
+
