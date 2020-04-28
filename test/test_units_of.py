ca39a09fa37a1e1bc8f03ab2db76a141b43e7029
Michael Clerx <MichaelClerx@users.noreply.github.com>
2020-04-28 11:47:06 +0100

    Update test/test_units_of.py
    Co-Authored-By: Jonathan Cooper <j.p.cooper@ucl.ac.uk>

--- test/test_units_of.py
+++ test/test_units_of.py
@@ -9,8 +9,7 @@ def test_unit_of():
 
     proto_file = 'test/protocols/test_units_of.txt'
     proto = fc.Protocol(proto_file)
-    proto.set_output_folder('test_unit_of')
+    proto.set_output_folder('test_units_of')
     proto.set_model('test/data/simple_ode.cellml')
     proto.run()
     # Test assertions are within the protocol itself
-
