e761ba21d94bea39775f98d14c1ec205e03b8a18
Michael Clerx <michael.clerx@cs.ox.ac.uk>
2020-05-07 16:03:45 +0100

    Absent library or protocol sections are now treated as empty statement lists. Closes #27.

--- test/test_empty_sections.py
+++ test/test_empty_sections.py
@@ -8,6 +8,6 @@ def test_empty_sections():
 
     proto = fc.Protocol('test/protocols/test_empty_sections.txt')
     proto.set_output_folder('test_empty_sections')
-    proto.set_model('test/models/simple_ode_model.cellml')
+    proto.set_model('test/models/simple_ode.cellml')
     proto.run()
 
